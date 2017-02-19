5176fcb37c70b90a26193e74be6342a28492abd8
Aman Kapur <akapur07@gmail.com>
2017-02-20 00:44:27 +0530

    Angular Factory.

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,9 +17,21 @@ grunt.initConfig({
     }
   }
   },
+  filerev: {
+    options: {
+      algorithm: 'md5',
+      length: 8
+    },
+    appbundle: {
+      src: 'app/dist/app.min.js',
+      dest : 'app/dist_min/'
+    }
+  }
 });
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.registerTask('default',['concat','uglify']);
+  grunt.loadNpmTasks('grunt-filerev');
+
+  grunt.registerTask('default',['concat','uglify','filerev']);
 
 }
