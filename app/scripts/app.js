a72909a9d47ed860bb81cc2d814217f6f2b871ce
Aman Codenvy <akapur07@gmail.com>
2017-02-24 05:40:09 +0000

    All the pages are transfered to views folder , ui-router added.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,23 +1,57 @@
 'use strict';
 
-angular.module('confusionApp', ['ngRoute'])
-    .config(function($routeProvider) {
-        $routeProvider
-            // route for the contactus page
-            .when('/contactus', {
-                templateUrl : 'contactus.html',
-                controller  : 'ContactController'
+angular.module('confusionApp', ['ui.router'])
+    .config(function($stateProvider,$urlRouterProvider){
+        $stateProvider
+            .state('app',{
+                url : '/',
+                views : {
+                    'header' : {
+                        templateUrl : 'views/header.html'
+                    },
+                    'content' : {
+                        template : '<h1>to be completed<h1>',
+                        controller : 'IndexController'
+                    },
+                    'footer' : {
+                        templateUrl : 'views/footer.html'
+                    }
+                }
             })
-            // route for the menu page
-            .when('/menu', {
-                templateUrl : 'menu.html',
-                controller  : 'MenuController'
+            
+            .state('app.aboutus',{
+                url : 'aboutus',
+                views : {
+                    'content@' : {
+                        template : '<h1>to be completed<h1>',
+                        controller : 'AboutController'
+                    }                }
             })
-            // route for the dish details page
-            .when('/menu/:id', {
-                templateUrl : 'dishdetail.html',
-                controller  : 'DishDetailController'
+            
+            .state('app.contactus',{
+                url : 'contactus',
+                views : {
+                    'content@' : {
+                        templateUrl : 'views/contactus.html',
+                        controller : 'ContactController'
+                    }                }
             })
-            .otherwise('/contactus');
-    })
-;
+            .state('app.menu',{
+                url : 'menu',
+                views : {
+                    'content@' : {
+                        templateUrl : 'views/menu.html',
+                        controller : 'MenuController'
+                    }                }
+            })
+            .state('app.dishdetails',{
+                url : 'menu/:id',
+                views : {
+                    'content@' : {
+                        templateUrl : 'views/dishdetail.html',
+                        controller : 'DishDetailController'
+                    }                }
+            });
+                        $urlRouterProvider.otherwise('/');
+
+    });
